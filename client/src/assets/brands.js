0906c0692b6890b93327760d6306bcf1e9731663
ABHIGYAN TRIPATHI <abhigyantripathi42@gmail.com>
2025-07-26 04:46:29 +0530

    Create brands.js

---
+++ client/src/assets/brands.js
@@ -0,0 +1,19 @@
+import google from './logos/google.png'
+import facebook from './logos/facebook.png'
+import netflix from './logos/netflix.png'
+import linkedin from './logos/linkedin.png'
+import paypal from './logos/paypal.png'
+import youtube from './logos/youtube.png'
+import amazon from './logos/amazon.png'
+import twitter from './logos/twitter.png'
+
+export const brandLogos = [
+  google,
+  facebook,
+  netflix,
+  linkedin,
+  paypal,
+  youtube,
+  amazon,
+  twitter,
+]
