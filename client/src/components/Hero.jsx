9c673f8e73d9c5e85c106fa457822bc35ccd4913
ABHIGYAN TRIPATHI <abhigyantripathi42@gmail.com>
2025-07-26 04:47:41 +0530

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
+import { brandLogos } from '../assets/brands'
 
-const Hero = () => {
+const Hero = ({scrollToRef}) => {
 
     const navigate = useNavigate()
+
+    const scrollToSection = () => {
+        if (scrollToRef?.current) {
+            const offset = -80; // Adjust this number as needed
+            const top = scrollToRef.current.getBoundingClientRect().top + window.scrollY + offset;
+            window.scrollTo({ top, behavior: 'smooth' });
+        }
+    // scrollToRef?.current?.scrollIntoView({ behavior: 'smooth' })
+  }
 
   return (
     <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen'>
@@ -16,11 +26,20 @@ const Hero = () => {
 
         <div className='flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs'>
             <button onClick={()=>navigate('/ai')} className='bg-primary text-white px-10 py-3 rounded-lg hover:scale-102 active:scale-95 transition cursor-pointer'>Start creating now</button>
-            <button className='bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer'>Watch demo</button>
+            <button onClick={scrollToSection} className='bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer'>Watch demo</button>
         </div>
 
         <div className='flex items-center gap-4 mt-8 mx-auto text-gray-600'>
             <img src={assets.user_group} alt="" className='h-8' /> Trusted by 10k+ people
+        </div>
+
+        {/* BRAND LOGOS SECTION */}
+        <div className="w-full overflow-hidden mt-20">
+            <div className="flex whitespace-nowrap animate-marquee items-center gap-14 w-max">
+                {[...brandLogos, ...brandLogos, ...brandLogos, ...brandLogos].map((logo, idx) => (
+                    <img key={idx} src={logo} alt="brand" className="h-6 sm:h-8 object-contain" />
+                ))}
+            </div>
         </div>
 
     </div>
